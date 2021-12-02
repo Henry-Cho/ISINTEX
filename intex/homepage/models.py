5140bcfaa13f4ad52cbe1d9c46b9bb8b03460600
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2021-12-02 10:57:18 -0700

    pres model

--- intex/homepage/models.py
+++ intex/homepage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Prescriber(models.Model):
+    fname = models.CharField(max_length=11, blank=False)
+    lname = models.CharField(max_length=11, blank=False)
+    gender = models.CharField(max_length=1, blank=False)
+    state = models.CharField(max_length=2, blank=False)
+    credentials = models.CharField(max_length=20, blank=True)
+    specialty = models.CharField(max_length=62, blank=False)
+    isopioidprescriber = models.BooleanField(default=False)
+    totalprescriptions = models.IntegerField(blank=False)
+
+    class Meta:
+        db_table = "pd_Prescriber"
+    
+    def __str__(self):
+        return (self.fname)
