399115e4efa4e9cb6d3b97913e8a8bb1e9d1aa45
Hyungseok Cho <hyungseokcho@Hyungseoks-MacBook-Pro.local>
2021-12-02 11:33:52 -0700

    Merge with Cole

--- intex/homepage/views.py
+++ intex/homepage/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.apps import apps
+from .models import Drug
+
+# Drug = apps.get_model('homepage','Drug')
 
 # Create your views here.
 def indexPageView(request) :
     return render(request, 'homepage/index.html')
+
+def drugViewPage(req) :
+    data = Drug.objects.all()
+
+    context = {
+        'druglist': data,
+    }
+    return render(req, 'homepage/drug.html', context)
+
+def drugDetailViewPage(req, id) :
+
+    record = Drug.objects.get(id= id)
+
+    context = {
+        'drug': record
+    }
+
+    return render(req, 'homepage/drugdetail.html', context)
 
