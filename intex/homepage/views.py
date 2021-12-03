d595fd70e48f8849961748fe5c56aa2bdf89a0d9
Hyungseok Cho <hyungseokcho@Hyungseoks-MacBook-Pro.local>
2021-12-03 04:13:14 -0700

    before activating filtering

--- intex/homepage/views.py
+++ intex/homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.apps import apps
-from .models import Drug
+from .models import Drug, Prescriber
 
 # Drug = apps.get_model('homepage','Drug')
 
@@ -20,9 +20,59 @@ def drugDetailViewPage(req, id) :
 
     record = Drug.objects.get(id= id)
 
+    drugName = record.drugname.lower().replace('.', '')
+
+    print(drugName)
+
+    sQuery = f'SELECT id, fname, lname, {drugName} AS drugQty FROM pd_prescriber '
+
+    if (drugName != '') :
+        sQuery += f'ORDER BY drugQty DESC LIMIT 10;'
+    
+    prescriberList = Prescriber.objects.raw(sQuery)
+
     context = {
-        'drug': record
+        'drug': record,
+        'prescriber': prescriberList
     }
 
     return render(req, 'homepage/drugdetail.html', context)
 
+def searchDrug(req):
+    input = req.GET['search']
+    input2 = req.GET.get('filter')
+
+    tf = False
+
+    if input != 'on' :
+        tf = True
+
+    print(input2)
+    print(tf)
+
+    druglist = Drug.objects.filter(drugname__icontains=input, isopioid= tf)
+    context = {
+        'drug': druglist
+    }
+
+    return render(req, 'homepage/drugsearch.html', context)
+
+# def searchEmpPageView(request) :
+#     sFirst = request.GET['first_name']
+#     sLast = request.GET['last_name']
+#     sGrade_Level = request.GET['grade_level']
+#     sQuery = 'SELECT Student.id, first_name, last_name, description FROM Student, Grade_Level WHERE Student.class_level_id = Grade_Level.class_level'
+#     if sFirst != '' :
+#         sQuery += " AND first_name = '" + sFirst + "'"
+#     if sLast != '' :
+#         sQuery += " AND last_name = '" + sLast + "'"
+#     if sGrade_Level != '' :
+#         sQuery += " AND class_level_id = '" + sGrade_Level + "'"        
+#     sQuery += ' ORDER BY first_name, last_name, description'
+#     data = Student.objects.raw(sQuery)
+#     lookup = Grade_Level.objects.all()
+#     context = {
+#         "our_students" : data,
+#         "grades" : lookup,
+#     }
+#     return render(request, 'travelpages/displayStudents.html', context) 
