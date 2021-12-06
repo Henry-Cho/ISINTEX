5ba96268b12b24e9daadde9895efd6fa77ed6bad
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2021-12-05 23:40:39 -0700

    analysis descriptions

--- intex/intex/settings.py
+++ intex/intex/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'intex',
         'USER': 'postgres',
-        'PASSWORD': 'group5',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
 
     }
